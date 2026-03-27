61633943a181159334e4a70cc3f73193c6a27540
Victor Farias <60015667+AHRGO@users.noreply.github.com>
2026-03-27 15:05:44 +0000

    creates script to build flutter easy

---
+++ scripts/gfg.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# gfg means - Go Flutter Go. Tribute to the music "Jhonny B Goode"
+
+# this script automatizes flutter rebuild on windows. It's very specific to my actual workplace
+
+## starting text
+prints_text_in_cyan() {
+    printf "\e[36m$1\e[0m"
+}
+
+## succesful text
+prints_text_in_green() {
+    printf "\e[32m$1\e[0m"
+}
+
+prints_starting_message() {
+    printf "\n"
+    prints_text_in_cyan "Starting $1..."
+    printf "\n\n"
+}
+
+prints_successful_message() {
+    printf "\n"
+    prints_text_in_green "$1 has successfully run!"
+    printf "\n\n"
+}
+
+calls_flutter_clean() {
+    local function_name="Flutter Clean"
+
+    prints_starting_message "$function_name"
+    fvm flutter clean
+    prints_successful_message "$function_name"
+}
+
+calls_flutter_pub_get_with_fvm() {
+    local function_name="Flutter Pub Get"
+
+    prints_starting_message "$function_name"
+    fvm flutter pub get
+    prints_successful_message "$function_name"
+}
+
+calls_dart_run_build_runner() {
+    local function_name="Flutter Run Build Runner"
+
+    prints_starting_message "$function_name"
+    fvm dart run build_runner build --delete-conflicting-outputs
+    prints_successful_message "$function_name"
+}
+
+gfg() {
+    calls_flutter_clean
+    calls_flutter_pub_get_with_fvm
+    calls_dart_run_build_runner
+}
+
+gfg
